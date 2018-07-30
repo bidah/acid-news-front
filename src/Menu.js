aa0d2dd371bf2d51a2f61b5c5d6ab55656a9fc3a
bidah <bartoindahouse@gmail.com>
2018-07-30 17:04:01 -0400

    msg when saved

--- src/Menu.js
+++ src/Menu.js
@@ -1,13 +1,19 @@
 import React, { Component, Fragment } from 'react';
 import './Menu.css';
+import { Link } from 'react-router-dom'
 
 class Menu extends Component {
 
   render() {
     return (
-      <ul>
-        <li>Home</li>
-        <li>Read Later Items</li>
+      <ul className="menu">
+        <Link to="/">
+          <li>Home</li>
+        </Link>
+
+        <Link to="/read-later">
+          <li>Read Later Items</li>
+        </Link>
       </ul>
     );
   }
